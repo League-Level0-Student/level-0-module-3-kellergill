bd48f95279a0af0a8a217cb949d25390cc55ed67
gill.keller <gill.keller@SEMS-259-17.SEMS259.LAB>
2018-03-07 14:53:50 -0800

    today we did fizzbuzz and bullseye

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,7 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -26,6 +27,18 @@ package modulo;
 **/
 
 public class FizzBuzz {
-
+public static void main(String[] args) {
+	for (int i = 1; i < 21; i++) {
+		System.out.println(i);
+		if(i%3==0) {
+		System.out.println("Fizz");
+		if(i%5==0) {
+		System.out.println("Buzz");
+		if(i>20&&i<30) {
+		System.out.println("FizzBuzz");
+		}
+		}
+	}
 }
-
+}
+}
