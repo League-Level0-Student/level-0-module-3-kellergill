99e306acedfb94642af59bd69991903a9588e8ae
gill.keller <gill.keller@SEMS-259-17.SEMS259.LAB>
2018-03-21 14:59:30 -0700

    today we did obedient robot

--- src/elseif/ObedientRobot.java
+++ src/elseif/ObedientRobot.java
@@ -1,5 +1,7 @@
 package elseif;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
@@ -8,6 +10,29 @@ public static void main(String[] args) {
 	 Chocolate.setSpeed(2100000);
 	 Chocolate.penDown();
 	 
+	 
+	 
+String Shap = 	 JOptionPane.showInputDialog(null, "wat shap 2 darw????????????????????????");
+	 
+	 if(Shap.equals("Square")) {
+		 drawSquare();
+	 }
+	 
+	 if(Shap.equals("Triangle")) {
+		 drawTriangle();
+	 }
+	 
+	 if(Shap.equals("Circle")) {
+		 drawCircle();
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 }
 public static void drawSquare() {
 	for (int i = 0; i < 4; i++) {
@@ -15,5 +40,18 @@ public static void drawSquare() {
 		Chocolate.turn(360/4);
 	}
 }
-
+public static void drawTriangle() {
+	for (int i = 0; i < 3; i++) {
+		Chocolate.move(100);
+		Chocolate.turn(360/3);
+		
+	}
+}
+public static void drawCircle() {
+	for (int i = 0; i < 360; i++) {
+		Chocolate.move(1);
+		Chocolate.turn(360/360);
+		
+	}
+}
 }
